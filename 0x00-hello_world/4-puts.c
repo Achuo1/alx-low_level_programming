527e346a2d0d5e566af3a9847a45a7df9c7871be
Achuo1 <achuoanangstanislaus@gmail.com>
2023-03-17 02:28:05 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,10 @@
  * main-this is the main function
  *
  * return: always 0
+ */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle);
+	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
