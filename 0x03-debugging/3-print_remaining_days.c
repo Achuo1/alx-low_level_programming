88002297cee768007a337b9e11510e5c841477e5
Achuo1 <achuoanangstanislaus@gmail.com>
2023-03-23 18:19:43 +0000

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -13,9 +13,10 @@
 void print_remaining_days(int month, int day, int year)
 {
 	int days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
 	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
 	{
-		days_in_month[2] = 29; // February has 29 days in a leap year
+		days_in_month[2] = 29;
 	}
 
 	if (month < 1 || month > 12 || day < 1 || day > days_in_month[month])
@@ -25,6 +26,7 @@ void print_remaining_days(int month, int day, int year)
 	}
 
 	int day_of_year = day;
+
 	for (int i = 1; i < month; i++)
 	{
 		day_of_year += days_in_month[i];
@@ -32,16 +34,15 @@ void print_remaining_days(int month, int day, int year)
 
 	if (days_in_month[2] == 29 && month > 2)
 	{
-		day_of_year++; // Add one day for the leap year
+		day_of_year++;
 	}
 
 	int days_left = 365;
+
 	if (days_in_month[2] == 29)
 	{
 		days_left = 366;
 	}
-	
 	printf("Day of the year: %d\n", day_of_year);
 	printf("Remaining days: %d\n", days_left - day_of_year);
 }
-
