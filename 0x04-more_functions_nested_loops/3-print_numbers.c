4efc9f314e798f41eb19398b9e934f55d169b001
Achuo1 <achuoanangstanislaus@gmail.com>
2023-03-25 01:19:14 +0000

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,17 +2,16 @@
 
 /**
  * print_numbers - print from 1 to 9
- * @i: parameter under test
  * Return: 0 always
  */
 
 void print_numbers(void)
 {
-	int i;
+	int c;
 
-	for (i = 0 ; i < 10 ; i++)
+	for (c = 0 ; c < 10 ; c++)
 	{
-		_putchar(i + '0');
+		_putchar(c + '0');
 	}
 	_putchar('\n');
 }
