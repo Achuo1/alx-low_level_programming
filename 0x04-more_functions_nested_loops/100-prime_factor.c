2c7924df6a18a5b6f26d74d4cb26c7bc2eb693d3
Achuo1 <achuoanangstanislaus@gmail.com>
2023-03-25 02:54:54 +0000

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio,h>
+#include "main.h"
+
+/**
+ * main - function to do
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	long x, maxf;
+	long number = 012852475143;
+	double square = sqrt(number);
+
+	for (x = 1; x <= square; x++)
+	{
+		if (number % x == 0)
+		{
+			maxf = number / x;
+		}
+	}
+	printf("%d\n", maxf);
+	return (0);
+}
